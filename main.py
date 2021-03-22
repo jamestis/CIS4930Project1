4b0f9b546d414a0cf9302719be54c3531a386cec
James Tison <jamestison@JamesMac.local>
2021-03-22 17:24:57 -0400

    Input handling

--- main.py
+++ main.py
@@ -5,37 +5,40 @@
 #outputs = avg rtt
 
 import sys
-
+import numpy
+#contains event information
 class Event:
     def __init__(self, arrival_time, size, id,):
         self.arrival_time = arrival_time
         self.size = size
         self.id = id
 
-if len(sys.argv) != 7:
-    print("Program requires 6 command line arguments")
-    exit()
-
-#require number values
-#order : lambda, num files, mu_s, k_s, mu_p, k_p,
-for i in range(1,7):
-    try:
-        float(sys.argv[i])
-        print(float(sys.argv[i]))
-    except ValueError:
-        print("All arguments must be convertable to type float.")
+#input validation
+def check_inputs(args):
+    if len(sys.argv) != 7:
+        print("Program requires 6 command line arguments")
         exit()
+    for i in range(1,7):
+        try:
+            float(sys.argv[i])
+            print(float(sys.argv[i]))
+        except ValueError:
+            print("All arguments must be convertable to type float.")
+            exit()
 
-lamb = float(sys.argv[1])
-num_files = int(sys.argv[2])
-mu_s = float(sys.argv[3])
-if not (mu_s > 1):
-    print("mu_s must be greater than 1.")
-    exit()
-k_s = float(sys.argv[4])
-mu_p = float(sys.argv[5])
-if not (mu_p > 1):
-    print("mu_p must be greater than 1.")
-t = Event(0,0,0)
-print(t.size)
+    lamb = float(sys.argv[1])
+    num_files = int(sys.argv[2])
+    mu_s = float(sys.argv[3])
+    if not (mu_s > 1):
+        print("mu_s must be greater than 1.")
+        exit()
+    k_s = float(sys.argv[4])
+    mu_p = float(sys.argv[5])
+    if not (mu_p > 1):
+        print("mu_p must be greater than 1.")
+        exit()
+    k_p = float(sys.argv[6])
+    return [lamb,num_files, mu_s, k_s, mu_p, k_p]
+
+
 exit()
